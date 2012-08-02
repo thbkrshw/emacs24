aa27387346c06248bcc98094a28e9d48fc69a89a
Harold Leboulanger <harold@leboulanger.org>
2012-08-02 13:35:01 +0200

    Add autoload php-mode

--- hl-package.el
+++ hl-package.el
@@ -23,4 +23,10 @@
 (setq smex-save-file (concat user-emacs-directory ".smex-items"))
 (smex-initialize)
 
+
+;; Setup php-mode
+(autoload 'php-mode "php-mode" "Major mode for editing php code." t)
+(add-to-list 'auto-mode-alist '("\\.php$" . php-mode))
+
+
 (provide 'hl-package)
