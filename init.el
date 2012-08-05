b7480a9e90a6993bf0018ea3bbe06833d0b42368
Harold Leboulanger <harold.leboulanger@gmail.com>
2012-08-05 18:14:46 +0200

    Comment proxy

--- init.el
+++ init.el
@@ -1,22 +1,11 @@
-;; Need connectivity to setup packages
-(setq proxy (concat user-emacs-directory "proxy.el"))
-(when (file-exists-p proxy) (load proxy))
+;;(setq url-proxy-services '(("no_proxy" . "work\\.com")
+;;                           ("http" . "p-goodway:3128")))
 
-
-;; (cd "~/.emacs.d")
+(cd "~/.emacs.d")
 (add-to-list 'load-path "~/.emacs.d")
-
 ;; Hide GUI Stuff early
-(dolist (mode '(tool-bar-mode scroll-bar-mode))
+(dolist (mode '(menu-bar-mode tool-bar-mode scroll-bar-mode))
   (when (fboundp mode) (funcall mode -1)))
 
 (mapc 'require '(uniquify hl-package hl-misc hl-defuns hl-win hl-keys))
-
-
-;; Load 
-(setq system-config (concat user-emacs-directory system-name ".el"))
-(when (file-exists-p system-config) (load system-config))
-
-;;(setq url-proxy-services '(("no_proxy" . "work\\.com")
-;;                          ("http" . "p-goodway:3128")))
 
