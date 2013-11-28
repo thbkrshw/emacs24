c139c54ad7eb28889f0707445d6fb46174ee9bd1
Harold Leboulanger <harold.leboulanger@gmail.com>
2013-11-28 09:00:06 +0100

    Disable autopair-mode for web-mode

--- hl-package.el
+++ hl-package.el
@@ -77,3 +77,8 @@
 (add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.twig\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.html\\'" . web-mode))
+
+;; Disable autopair for web-mode
+(add-hook 'web-mode-hook
+	  #'(lambda ()
+	      (autopair-mode -1)))
