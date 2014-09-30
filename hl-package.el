f2317b17094d52afb582cc0e843ad4a8b989888f
Harold Leboulanger <harold.leboulanger@gmail.com>
2014-09-30 16:14:25 +0200

    Add helm shortcuts

--- hl-package.el
+++ hl-package.el
@@ -96,6 +96,14 @@
 (global-set-key (kbd "C-c h") 'helm-command-prefix)
 (global-unset-key (kbd "C-x c"))
 
+(global-set-key (kbd "M-x") 'helm-M-x)
+(global-set-key (kbd "M-y") 'helm-show-kill-ring)
+(global-set-key (kbd "C-x b") 'helm-mini)
+(global-set-key (kbd "C-x C-f") 'helm-find-files)
+(global-set-key (kbd "C-c h o") 'helm-occur)
+
+
+(helm-mode 1)
 
 ;; Projectile
 
