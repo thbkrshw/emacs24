795ea427f6d2d73307519d1f8e46d58a72955165
Harold Leboulanger <harold.leboulanger@gmail.com>
2014-10-18 13:16:37 +0200

    Remove ido

--- hl-misc.el
+++ hl-misc.el
@@ -1,22 +1,10 @@
 (show-paren-mode 1)
 (setq visible-bell 'top-bottom)
 (setq inhibit-startup-screen t)
-
-(require 'ido)
-;(require 'ido-ubiquitous)
-(ido-mode t)
-;(ido-ubiquitous t)
-(setq ido-enable-prefix nil
-      ido-enable-flex-matching t
-      ido-auto-merge-work-directories-length nil
-      ido-create-new-buffer 'always
-      ido-use-filename-at-point 'guess
-      ido-use-virtual-buffers t
-      ido-handle-duplicate-virtual-buffers 2
-      ido-max-prospects 10)
+(setq initial-scratch-message nil)
 
 ;; Abbreviation
-(setq save-abbrevs t)
+;;(setq save-abbrevs t)
 
 ;; Ace jump mode
 (autoload 'ace-jump-mode "ace-jump-mode" "Emacs quick move minor mode" t)
@@ -34,8 +22,8 @@
    backup-directory-alist
     '(("." . "~/.emacs.d/backup"))    ; don't litter my fs tree
    delete-old-versions t
-   kept-new-versions 6
-   kept-old-versions 2
+   kept-new-versions 1
+   kept-old-versions 1
    version-control t)       ; use versioned backups
 
 
@@ -50,13 +38,6 @@
 	     (0 (progn (compose-region (match-beginning 1)
 				       (match-end 1) "\u0192")
 		       nil)))))
-
-
-
-
-
-;; (setq smex-save-file (concat user-emacs-directory ".smex-items"))
-;; (smex-initialize)
 
 
 ;; Uniquify conf
