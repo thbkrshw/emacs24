8de59bbc08438a1a6c27d6f1df4b0b72f4c20d13
Harold Leboulanger <harold.leboulanger@gmail.com>
2015-01-03 19:07:54 +0100

    Remove old repo

--- lisp/hl-package.el
+++ lisp/hl-package.el
@@ -2,10 +2,6 @@
 
 (add-to-list 'package-archives
              '("melpa" . "http://melpa.milkbox.net/packages/"))
-(add-to-list 'package-archives
-	     '("ELPA" . "http://tromey.com/elpa/"))
-(add-to-list 'package-archives
-             '("marmalade" . "http://marmalade-repo.org/packages/"))
 
 (package-initialize)
 
