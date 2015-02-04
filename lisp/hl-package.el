4bf951758a28b059513c1759eb0916884efb2105
Harold Leboulanger <harold.leboulanger@gmail.com>
2015-02-04 22:15:53 +0100

    better helm shortcuts

--- lisp/hl-package.el
+++ lisp/hl-package.el
@@ -89,6 +89,10 @@
 (global-set-key (kbd "C-c h") 'helm-command-prefix)
 (global-unset-key (kbd "C-x c"))
 
+(define-key helm-map (kbd "<tab>") 'helm-execute-persistent-action) ; rebind tab to run persistent action
+(define-key helm-map (kbd "C-i") 'helm-execute-persistent-action) ; make TAB works in terminal
+(define-key helm-map (kbd "C-z")  'helm-select-action) ; list actions using C-z
+
 (global-set-key (kbd "M-x") 'helm-M-x)
 (global-set-key (kbd "M-y") 'helm-show-kill-ring)
 (global-set-key (kbd "C-x b") 'helm-mini)
